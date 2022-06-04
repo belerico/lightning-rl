af65fcaea4de92ca9d2ef507d167bcf57d32ce61
belerico_t <federico.belotti@orobix.com>
2022-06-04 13:10:25 +0200

    Fix names in main

--- main.py
+++ main.py
@@ -47,12 +47,12 @@ class A2CDemoFlow(L.LightningFlow):
         player_cfg: omegaconf.DictConfig,
         tester_cfg: omegaconf.DictConfig,
         trainer_cfg: omegaconf.DictConfig,
-        num_agents: int = 2,
+        num_players: int = 2,
         max_episodes: int = 1000,
         rendering_frequency: int = 10,
     ):
         super().__init__()
-        self.num_agents = num_agents
+        self.num_players = num_players
         self.max_episodes = max_episodes
         self.rendering_frequency = rendering_frequency
         input_dim, action_dim = Player.get_env_info(player_cfg.environment_id)
@@ -72,10 +72,10 @@ class A2CDemoFlow(L.LightningFlow):
             parallel=True,
         )
         self.players = PlayersFlow(
-            self.num_agents, player_cfg, model_state_dict_path=self.trainer.model_state_dict_path
+            self.num_players, player_cfg, model_state_dict_path=self.trainer.model_state_dict_path
         )
         self.logger = TensorboardWork("./logs", num_agents=1, parallel=True)
-        self.hello_component = LitStreamlit(rendering_path=self.tester.rendering_path)
+        self.gif_renderer = LitStreamlit(rendering_path=self.tester.rendering_path)
 
     def run(self):
         self.players.run(self.trainer.episode_counter)
@@ -90,11 +90,11 @@ class A2CDemoFlow(L.LightningFlow):
 
     def configure_layout(self):
         tab_1 = {"name": "TB logs", "content": self.logger.url}
-        tab_2 = {"name": "GIF", "content": self.hello_component}
+        tab_2 = {"name": "GIF", "content": self.gif_renderer}
         return [tab_1, tab_2]
 
 
 if __name__ == "__main__":
     with initialize(config_path="./demos/a2c_demo/configs/"):
         config = compose(config_name="config.yaml")
-        app = L.LightningApp(A2CDemoFlow(config.player, config.tester, config.trainer, num_agents=1, max_episodes=500))
+        app = L.LightningApp(A2CDemoFlow(config.player, config.tester, config.trainer, num_agents=2, max_episodes=500))
